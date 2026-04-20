6ba88f4bff9de22010b4d1a8e588e3e7fdfdcaa2
s32806 <s32806@pjwstk.edu.pl>
2026-04-20 15:33:44 +0200

    Initial commit

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 5f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+
+    {
+
+        float moveInput = Input.GetAxis("Horizontal");
+
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+
+        {
+
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+        }
+    }
+}
