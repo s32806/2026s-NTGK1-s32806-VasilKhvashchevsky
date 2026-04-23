26e9402f7992434ce47f92b93effc18825bd3ea7
s32806 <s32806@pjwstk.edu.pl>
2026-04-23 19:36:54 +0300

    Initial commit

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -2,30 +2,57 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float jumpForce = 5f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 8f;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
 
     private Rigidbody2D rb;
+    private Animator an;
+    private SpriteRenderer sr;
 
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        an = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
-    void Update()
+    private void Update()
 
     {
 
         float moveInput = Input.GetAxis("Horizontal");
 
-        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+        rb.velocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
 
-
-        if (Input.GetKeyDown(KeyCode.Space))
-
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
-
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+        UpdateAnimations();
+        Flip(moveInput);
+    }
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(
+            groundCheck.position,
+            Vector2.down,
+            groundCheckDistance,
+            groundLayer
+        );
+        return hit.collider != null;
+    }
+    private void UpdateAnimations() {
+            an.SetFloat("XMovement", Mathf.Abs(rb.velocity.x));
+            an.SetFloat("YMovement", Mathf.Abs(rb.velocity.y));
+    }
+    private void Flip(float moveInput) {
+            if (moveInput > 0) {
+                sr.flipX = false;
+            } else if (moveInput < 0) {
+                sr.flipX = true;
+            }
     }
 }
